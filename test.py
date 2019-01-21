8b77dcde7c1f25932d45f8a8f645d5fb8a25e670
zhangslob <zhangslob@gmail.com>
2019-01-21 22:45:15 +0800

    feat: reflash cookies

--- test.py
+++ test.py
@@ -14,15 +14,16 @@ from datetime import datetime
 headers = {
     'origin': 'https://yuba.douyu.com',
     'accept-encoding': 'gzip, deflate, br',
-    'x-csrf-token': '6VpG4UPcgq4mx2cDPCmn5W9UMkOM2VzB',
+    'x-csrf-token': 'j5eOSHZcnFwQxVZlF9wKx3vFTL4Ddxzn',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
     'content-type': 'application/x-www-form-urlencoded',
     'accept': '*/*',
     'referer': 'https://yuba.douyu.com/p/580027761547043070',
     'authority': 'yuba.douyu.com',
-    'cookie': 'dy_did=a17b4ab25c54986f6433ea9b00061501; acf_yb_did=a17b4ab25c54986f6433ea9b00061501; _ga=GA1.2.898345405.1546830910; _dys_lastPageCode=page_yuba_detail,page_yuba_detail; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1545465128,1546395515,1546839422; smidV2=20181222155555157e437e6e4f57b560a3855ffdf9aace007e26097861818f0; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1547175937; Hm_lvt_e0374aeb9ac41bee98043654e36ad504=1547087949,1547089028,1547100500,1547175944; _gid=GA1.2.644898675.1547446717; acf_yb_auth=ff5b0def5969878f652de1d2c8b40309403b4ac1; acf_yb_new_uid=qy70WlRPJAXG; acf_yb_uid=39581553; acf_yb_t=6VpG4UPcgq4mx2cDPCmn5W9UMkOM2VzB; _dys_refer_action_code=init_page_yuba_detail; Hm_lpvt_e0374aeb9ac41bee98043654e36ad504=1547446728',
+    'cookie': 'dy_did=a17b4ab25c54986f6433ea9b00061501; acf_yb_did=a17b4ab25c54986f6433ea9b00061501; _ga=GA1.2.898345405.1546830910; smidV2=20181222155555157e437e6e4f57b560a3855ffdf9aace007e26097861818f0; wan_auth37wan=e1abd8a99355CdiQ5cPAr8BtuD1mA25ipDS2Skn7VXPUoi1YHCRc5FEgePtW5GVRGfXdLu3hBeq8mcK%2BF1m8tpLA8prQ2%2BY1iaiH%2FB%2FOPw6wQ3RAEQ; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1546395515,1546839422,1547555261; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1548080430; _gid=GA1.2.425428586.1548080434; _gat_gtag_UA_128072138_1=1; Hm_lvt_e0374aeb9ac41bee98043654e36ad504=1547100500,1547175944,1547555270,1548080434; acf_yb_t=j5eOSHZcnFwQxVZlF9wKx3vFTL4Ddxzn; acf_yb_auth=463a89a60f1d089389926b192bbde0e302758ec3; acf_yb_new_uid=qy70WlRPJAXG; acf_yb_uid=39581553; _dys_refer_action_code=init_page_yuba_detail; _dys_lastPageCode=page_yuba_detail,page_yuba_detail; Hm_lpvt_e0374aeb9ac41bee98043654e36ad504=1548080439',
 }
+
 
 params = (
     ('timestamp', '0.8552529192809504'),
@@ -54,4 +55,4 @@ def run():
 if __name__ == '__main__':
     while True:
         run()
-        time.sleep(30)
+        time.sleep(10)
