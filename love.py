eca8c2245139f05e9452c85e516fbfc430323e51
zhangslob <zhangslob@gmail.com>
2019-02-21 17:15:18 +0800

    add love.py

---
+++ love.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+"""
+@author: zhangslob
+@file: love.py 
+@time: 2019/02/21
+@desc: 
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
