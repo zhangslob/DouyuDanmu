f2679f61f359ca3ef34e1c3d382d7f8acb21908e
zhangslob <zhangslob@gmail.com>
2019-02-22 14:04:54 +0800

    add uwsgi

---
+++ run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+"""
+@author: zhangslob
+@file: run.py
+@time: 2019/02/22
+@desc: 
+"""
+
+from love import app
+
+
+if __name__ == '__main__':
+    app.run()
